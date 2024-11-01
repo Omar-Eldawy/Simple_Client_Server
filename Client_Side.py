10954acd671348cc353c9f69cc74635d4e8994d5
Omar-Eldawy <eng.omar.aldawy@gmail.com>
2024-11-01 03:56:46 +0200

    client_can_do_many_requests_in_one_connection

--- Client_Side.py
+++ Client_Side.py
@@ -3,41 +3,39 @@ import os
 
 class Client:
     def __init__(self, host: str, port: int):
-        self.host = host
-        self.port = port
-        self.client = None
+        self.__host = host
+        self.__port = port
+        self.__client = None
 
     def action(self, command: str, path: str) -> None:
         if command == 'POST':
-            self.post_file(path)
+            self.__post_file(path)
         elif command == 'GET':
-            self.get_file(path)
+            self.__get_file(path)
         else:
             print('Invalid Command')
 
-        self.client.close()
-
-    def post_file(self, path: str) -> None:
+    def __post_file(self, path: str) -> None:
         file_name = os.path.basename(path)
         with open(path, 'rb') as file:
             file_data = file.read()
         request_header = f"POST /{file_name} HTTP/1.1\r\nContent-Length: {len(file_data)}\r\n\r\n"
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.client:
-            self.client.connect((self.host, self.port))
-            self.client.sendall(request_header.encode('utf-8') + file_data)
-            response = self.client.recv(1024)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.__client:
+            self.__client.connect((self.__host, self.__port))
+            self.__client.sendall(request_header.encode('utf-8') + file_data)
+            response = self.__client.recv(1024)
             print(response.decode())
 
-    def get_file(self, file_name: str) -> None:
+    def __get_file(self, file_name: str) -> None:
         if not os.path.exists('Client_Directory'):
             os.makedirs('Client_Directory')
 
         request_header = f"GET /{file_name} HTTP/1.1\r\n\r\n"
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.client:
-            self.client.connect((self.host, self.port))
-            self.client.sendall(request_header.encode('utf-8'))
-            response = self.client.recv(1024)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.__client:
+            self.__client.connect((self.__host, self.__port))
+            self.__client.sendall(request_header.encode('utf-8'))
+            response = self.__client.recv(1024)
             print(response.decode())
 
             header, body = response.decode().split('\r\n\r\n', 1)
@@ -49,9 +47,16 @@ class Client:
                     file.write(body.encode())
                     print('File downloaded successfully')
 
+    def close(self) -> None:
+        self.__client.close()
+
 if __name__ == '__main__':
     server_port = 8080
     client = Client(socket.gethostbyname(socket.gethostname()), server_port)
-    client.action('GET', 'index.html')
-    client.action('GET', 'Adel.jpg')
-    client.action('POST', 'Emam.jpeg')
+    while True:
+        client_command = input('Enter Command: ')
+        if client_command == 'close':
+            break
+        file_path = input('Enter File Path: ')
+        client.action(client_command, file_path)
+    client.close()    
