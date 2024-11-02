ce1c83ec1fc7b3756edcfc07d114df19780828ea
Omar-Eldawy <eng.omar.aldawy@gmail.com>
2024-11-02 07:23:09 +0200

    close_and_file_not_found_fixed

--- Client_Side.py
+++ Client_Side.py
@@ -2,13 +2,13 @@ import socket
 import mimetypes
 import os
 
-# close isn't working
 
 class Client:
     def __init__(self, host: str, port: int):
         self.__host = host
         self.__port = port
-        self.__client = None
+        self.__client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__client.connect((self.__host, self.__port))
         self.__file_size = 20_971_520  # 20MB
 
     # directing the command to the appropriate function
@@ -26,6 +26,10 @@ class Client:
             os.makedirs('Client_Directory')
 
         file_name = os.path.basename(path)
+        if not os.path.exists(os.path.join('Client_Directory', file_name)):
+            print(f"Error: File '{file_name}' does not exist.")
+            return
+        
         # Guess the content type of the file
         content_type, _ = mimetypes.guess_type(file_name)
         with open(os.path.join('Client_Directory', file_name), 'rb') as file:
@@ -33,12 +37,10 @@ class Client:
         # Create the request header to send to the server
         request_header = f"POST /{file_name} HTTP/1.1\r\nContent-Length: {len(file_data)}\r\nContent-Type: {content_type}\r\n\r\n"
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.__client:
-            self.__client.connect((self.__host, self.__port))
-            # Send the request header and the file data to the server
-            self.__client.sendall(request_header.encode('utf-8') + file_data)
-            response = self.__client.recv(self.__file_size)
-            print(response.decode('utf-8'))
+        # Send the request header and the file data to the server
+        self.__client.sendall(request_header.encode('utf-8') + file_data)
+        response = self.__client.recv(self.__file_size)
+        print(response.decode('utf-8'))
 
     def __get_file(self, file_name: str) -> None:
         # Check if the client directory exists, if not create it
@@ -47,30 +49,30 @@ class Client:
 
         # Create the request header to send to the server
         request_header = f"GET /{file_name} HTTP/1.1\r\n\r\n"
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.__client:
-            self.__client.connect((self.__host, self.__port))
-            # Send the request header to the server to get the file/img
-            self.__client.sendall(request_header.encode('utf-8'))
-            response = self.__client.recv(self.__file_size)
 
-            header_end = response.find(b'\r\n\r\n')
-            if header_end == -1:
-                print('Error in response format')
-                return
+        # Send the request header to the server to get the file/img
+        self.__client.sendall(request_header.encode('utf-8'))
+        response = self.__client.recv(self.__file_size)
 
-            # Get the header and the body of the response separately
-            header = response[:header_end].decode('utf-8')
-            body = response[header_end + 4:]
+        header_end = response.find(b'\r\n\r\n')
+        if header_end == -1:
+            print('Error in response format')
+            return
 
-            if '404 Not Found' in header:
-                print('File not found on server')
-                return
-            else:
-                with open(os.path.join('Client_Directory', file_name), 'wb') as file:
-                    file.write(body)
-                    print('File downloaded successfully')
+        # Get the header and the body of the response separately
+        header = response[:header_end].decode('utf-8')
+        body = response[header_end + 4:]
+
+        if '404 Not Found' in header:
+            print('File not found on server')
+            return
+        else:
+            with open(os.path.join('Client_Directory', file_name), 'wb') as file:
+                file.write(body)
+                print('File downloaded successfully')
 
     def close(self) -> None:
+        self.__client.send("CLOSE".encode('utf-8'))
         self.__client.close()
 
 
@@ -79,13 +81,13 @@ if __name__ == '__main__':
     server_port = 8080
     client = Client(server_ip, server_port)
     while True:
-        client_command = input('Enter Command: ').upper()
-        if client_command == 'CLOSE':
-            client.close()
+        user_input = input('Enter Command: ')
+        client_command = user_input.split(' ', 1)
+        client_command[0] = client_command[0].upper()
+        if client_command[0] == 'CLOSE':
             break
-        if client_command not in ['POST', 'GET']:
+        if client_command[0] not in ['POST', 'GET']:
             print('Invalid Command')
             continue
-        file_path = input('Enter File Path: ')
-        client.action(client_command, file_path)
+        client.action(client_command[0], client_command[1])
     client.close()
