1baa2c639259e2bce36ce4acca9d1292d98510fd
Omar-Eldawy <eng.omar.aldawy@gmail.com>
2024-11-08 11:14:15 +0200

    client_reconnection

--- Client_Side.py
+++ Client_Side.py
@@ -110,22 +110,26 @@ class Client:
 if __name__ == '__main__':
     args = parse_args()
     client = Client(args.server_ip, args.port_number)
+    reconnecting_flag = False
+    commands = None
     while True:
-        file_path = input('Enter commands file path or Close to terminate:')
+        if not reconnecting_flag:
+            file_path = input('Enter commands file path or Close to terminate:')
 
-        if file_path.upper() == 'CLOSE':
-            client.send_close_message()
-            client.close()
-            print('Connection closed')
-            sys.exit(0)
+            if file_path.upper() == 'CLOSE':
+                client.send_close_message()
+                client.close()
+                print('Connection closed')
+                sys.exit(0)
 
-        check_file_path = validate_file(file_path)
-        if check_file_path != 'File is valid':
-            print(check_file_path)
-            continue
+            check_file_path = validate_file(file_path)
+            if check_file_path != 'File is valid':
+                print(check_file_path)
+                continue
 
-        commands = Utilities.read_file(file_path)
+            commands = Utilities.read_file(file_path)
 
+        reconnecting_flag = False
         for command in commands:
             client_command = Utilities.handle_command_parsing(command)
             if client_command[0].upper() == 'CLOSE':
@@ -137,6 +141,8 @@ if __name__ == '__main__':
                 print('Invalid Command')
                 continue
             if not client.action(client_command[0].upper(), client_command[1]):
-                client.close()
-                print('Connection closed')
-                sys.exit(0)
+                client = Client(args.server_ip, args.port_number)
+                reconnecting_flag = True
+                print('Reconnecting...', end='\n\n')
+                break
+
